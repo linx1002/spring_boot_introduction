86d7d7142c09ed3dc1f8edc06f86ec9f2f6abf6e
linx1002 <157979525+linx1002@users.noreply.github.com>
2024-02-14 15:34:31 +0900

    フォーマット調整

--- src/main/java/com/example/controller/LessonController.java
+++ src/main/java/com/example/controller/LessonController.java
@@ -15,52 +15,47 @@ import com.example.from.SampleForm;
 @Controller
 @RequestMapping("lesson")
 public class LessonController {
-	
+
 	@GetMapping("/sample")
-    //@ResponseBody
-    public String sample(Model model) {
+	//@ResponseBody
+	public String sample(Model model) {
 		String text = "Hello Spring Boot!!";
-		model.addAttribute("message",text);
-        return "index";
+		model.addAttribute("message", text);
+		return "index";
 	}
-	
-	 @GetMapping("/test/{num}")//Lesson2の例文04
-	    @ResponseBody
-	    public String test(@PathVariable Integer num) {
-	        return "Good Evening!" + num;
-	    }
-	 
-	 @GetMapping("/request_test")//03の例文
-	 @ResponseBody
-	 public String getTest(@RequestParam("name") String name
-			 ,@RequestParam("bloodType") String bloodType) {
-		 return "名前:" + name + "<br>血液型: " + bloodType;
-	 }
-	 
-	 public String postTest(SampleForm sampleForm) {
-		 return "名前:" + sampleForm.getName() + "<br>血液型: " + sampleForm.getBloodType();
-	 }
-	 
-	 @GetMapping("/form_test")
-	 public String formTest(SampleForm sampleForm, Model model) {
-		 model.addAttribute("SampleForm", sampleForm);
-		 return "lesson/form_test";
-	 }
-	 
-	 @PostMapping("/request_test")//05の例文
-	    @ResponseBody
-	    public String postTest(SampleForm sampleForm) {
-	        return "名前: " + sampleForm.getName() + "<br>血液型: " + sampleForm.getBloodType();
-	    }
-	 
-	 @GetMapping("/form_test")//07の例文
-	    public String formTest(@ModelAttribute SampleForm sampleForm) {
-	        return "lesson/form_test";
-	    }
-	 
-	 @GetMapping("/")//08の例文
-	 	public String test(@RequestParam("name") String name) {
-	    
-	 	}
-	 
+
+	@GetMapping("/test/{num}") //Lesson2の例文04
+	@ResponseBody
+	public String test(@PathVariable Integer num) {
+		return "Good Evening!" + num;
+	}
+
+	@GetMapping("/request_test") //03の例文
+	@ResponseBody
+	public String getTest(@RequestParam("name") String name, @RequestParam("bloodType") String bloodType) {
+		return "名前:" + name + "<br>血液型: " + bloodType;
+	}
+
+	@GetMapping("/form_test")
+	public String formTest(SampleForm sampleForm, Model model) {
+		model.addAttribute("SampleForm", sampleForm);
+		return "lesson/form_test";
+	}
+
+	@PostMapping("/request_test") //05の例文
+	@ResponseBody
+	public String postTest(SampleForm sampleForm) {
+		return "名前: " + sampleForm.getName() + "<br>血液型: " + sampleForm.getBloodType();
+	}
+
+	@GetMapping("/form_test") //07の例文
+	public String formTest(@ModelAttribute SampleForm sampleForm) {
+		return "lesson/form_test";
+	}
+
+	@GetMapping("/") //08の例文
+	public String test(@RequestParam("name") String name) {
+
+	}
+
 }
